0bed79f6279a0f1da6c64533414508e79eaded1f
mayank <mayankyad987@gmail.com>
2025-03-20 04:37:59 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let tl = gsap.timeline()
+function loaderanimation(){
+    let tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
    stagger:0.25,
@@ -34,12 +35,35 @@ tl.to("#loader",{
 tl.from("#page1",{
     y:1200,
     opacity:0,
-    duration:0.5,
-    delay:0.2
+    duration:0.3,
+    delay:0.1
     
 });
 
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
 
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+  y:120,
+  stagger:0.2
+});
+}
+
+function crsranimation(){
+    document.addEventListener('mousemove', function(dept){
+        gsap.to("#crsr",{
+          left:dept.x,
+          top:dept.y
+        })
+      });
+      
+      Shery.makeMagnet("#nav-part2 h4", {});
+}
+
+loaderanimation();
+
+crsranimation();
